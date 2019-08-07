c63a5e6e51d7310853c7cac391072abffc14b3f4
cleverwyq <yq_wang2002@hotmail.com>
2019-08-07 03:24:11 -0700

    no

--- src/main/java/HelloProducer.java
+++ src/main/java/HelloProducer.java
@@ -1,6 +1,7 @@
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
 
@@ -12,14 +13,18 @@ public class HelloProducer {
     public static void main(String[] args) {
 
         Properties properties = new Properties();
+        properties.put("client.id", "young_1");
         properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        //properties.put("request.timeout.ms", 30000122);
+        //properties.put("metadata.max.age.ms", 30000000);
         properties.put("bootstrap.servers", brokerList);
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, "Hello Kafka");
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, "key1", "Hello Kafka");
 
         try {
-            producer.send(record);
+            RecordMetadata rm = producer.send(record).get();
+            System.out.println(rm.toString());
         }
         catch(Exception e) {
             e.printStackTrace();
